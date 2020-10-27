1560b0a1e3726254886bb02a9f9d9f71762d6851
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-10-27 10:07:49 -0400

    adds review code.

---
+++ review/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import AppFunc from './AppFunc';
+import AppClass from './AppClass';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<AppFunc />, rootElement);
