25bf4360eda423cfbec4f97c788919e45c63b3da
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-10-27 12:27:52 -0400

    lecture work-in-progress

--- review/src/index.js
+++ review/src/index.js
@@ -5,4 +5,4 @@ import AppFunc from './AppFunc';
 import AppClass from './AppClass';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<AppFunc />, rootElement);
+ReactDOM.render(<AppClass />, rootElement);
