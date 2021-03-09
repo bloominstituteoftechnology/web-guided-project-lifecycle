b4e3c5ce5720ed9190bd82a2ddea8635333fb6da
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-03-09 11:07:43 -0500

    update pokemon

--- pokemon/src/components/Pokemon.js
+++ pokemon/src/components/Pokemon.js
@@ -5,14 +5,7 @@ function Pokemon(props) {
   return (
     <>
       {props.pokemon.map(pokemon => (
-        <div key={pokemon.id} className="pokemon">
-          <img src={pokemon.img} alt={pokemon.name} />
-          <div>
-            <h3>{pokemon.name}</h3>
-            {pokemon.next_evolution &&
-              pokemon.next_evolution.map(e => <p key={e.num}>{e.name}</p>)}
-          </div>
-        </div>
+        <Poki pokemon={pokemon}/>
       ))}
     </>
   );
---
+++ pokemon/src/components/Poki.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Poki extends React.Component {
+    render() {
+        const { pokemon } = props;
+
+        return(
+        <div key={pokemon.id} className="pokemon">
+          <img src={pokemon.img} alt={pokemon.name} />
+          <div>
+            <h3>{pokemon.name}</h3>
+            {pokemon.next_evolution &&
+              pokemon.next_evolution.map(e => <p key={e.num}>{e.name}</p>)}
+          </div>
+        </div>);
+    }
+}
