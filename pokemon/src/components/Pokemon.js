137b970ab57daf1cdcfdb6d520595ec7a5b7265d
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-06-01 16:32:48 -0400

    adds class on Pokemon.

--- pokemon/src/components/Pokemon.js
+++ pokemon/src/components/Pokemon.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Poki from './Poki';
 
-function Pokemon(props) {
-  return (
-    <>
-      {props.pokemon.map(pokemon => (
-        <Poki pokemon={pokemon}/>
-      ))}
-    </>
-  );
+class Pokemon extends React.Component {
+  render() {
+    return (
+      <>
+        {this.props.pokemon.map(pokemon => (
+          <Poki key={pokemon.id} pokemon={pokemon}/>
+        ))}
+      </>
+    );
+  }
 }
 
 export default Pokemon;
